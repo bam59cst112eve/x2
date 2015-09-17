a847750ba9fdf8defca0d768fcbddadf70b60407
Puled81 <drakep@verizon.net>
2015-09-16 20:06:26 -0400

    Update x2.java

--- x2.java
+++ x2.java
@@ -1,50 +1,67 @@
 //////// Exercise x2:  modularize exercise x1, and add dog to chase hero.
 //////// Drake  (CST 112; 9/16/15)
 
-String author=  "Drake";
-String title=  " Chasing Square ";
-String help=  " Click to relocate hero \n 'q' to quit; 'r' to reset. ";
+  String author=  "Drake";
+  String title=  " Chasing Dog ";
+  String help=  " Click to relocate hero \n 'q' to quit; 'r' to reset. ";
 
 
-//// GLOBALS:  coordinates, speed, etc.
-float horizon;
-float x, y;       // Position.
-float dx, dy;     // Speed.
-float dogX, dogY;
+  //// GLOBALS:  coordinates, speed, etc.
+  float horizon;
+  float x, y;       // Position.
+  float dx, dy;     // Speed.
+  float dogX, dogY;
 
-//// SETUP:  window size, initialization (start in middle of screen).
-void setup() {
-  size( 640,480);
+  //// SETUP:  window size, initialization (start in middle of screen).
+  void setup() {
+  size( 700,700);
   horizon=  height/4;
   x=  width/2;
   y=  height/2;
   dx=  3;
   dy=  2;
-}
+  }
 
-//// NEXT FRAME:  scene, action, show.
-void draw() {
+  //// NEXT FRAME:  scene, action, show.
+  void draw() {
   scene();
   hero();
   dog();
   messages();
-}
+  }
 
-//// SCENE:  sky, sun, tree, house, etc.
-void scene() {  
-  /* INSERT YOUR CODE HERE! */    background( 200,255,255 );
+  //// SCENE:  sky, sun, tree, house, etc.
+  void scene() {  
+background( 100,200,250 );                // sky
+  fill( 255,255,0 );
+  
+  fill( 100,200,100 );
+  rect( 0,horizon, width,height*3/4 );      // grass.
+  
+  triangle(550,150,600,150,575,100);
+  fill(139,80,13);
+  rect(568,150,15,25);
+  
+  
+  fill(255,0,0);
+  rect( 150,horizon, 120,horizon-50);  // house
+  fill(139,80,13);
+  rect(230,250,25,50);
   fill(0);
-  /* REPLACE THIS STUB! */  text( "scene", 100, 100 );
-}
+  triangle(150,horizon,271,horizon,210,90);
+  fill(255);
+  rect(165,200,30,30);
+  rect(225,200,30,30);
+  }
 
-void messages() {
+  void messages() {
   text( title, width/3, 20 );
   text( help, width*2/3, 30 );
   text( author, 10,height-20 );
-}
+  }
 
-//// ACTION:  move (x,y) coordinates of hero & dog; show them.
-void hero() {
+  //// ACTION:  move (x,y) coordinates of hero & dog; show them.
+  void hero() {
   fill(50,80,150); 
   rect( x,y, 30,50 );
   fill(255,0,0);
@@ -60,32 +77,32 @@ void hero() {
   if (x<0) dx=-dx;
   y= y+dy;
   if (y<0 || y>height-80){ dy=-dy;}
-}
-void dog() {
+  }
+  void dog() {
   dogX=  dogX - (dogX-x)/30;
   dogY=  dogY - (dogY-y)/40;
   text( dogX, 10, 10 );
   text( dogY, 10, 20 );
   //
   fill( 150,0,0 );
-  //rect(dogX,dogY, 50,30 );
+  //rect(dogX,dogY, 50,30 ); change shape of dog
   ellipse(dogX,dogY,50,30);
   /* INSERT YOUR CODE HERE! */
-  /* REPLACE THIS STUB! */  text( "woof, woof!d!!", 150, 150 );
-}
+  text( "Arf,Arf!", dogX-20, dogY-20);
+  }
 
 //////// HANDLERS:  mouse clicks, keys
-void mousePressed() {
+  void mousePressed() {
   x=  mouseX;         // Set (x,y) to mouse
   y=  mouseY;
   //
   dx=  random( -6, +6 );                  // random speed.
   dy=  random( -4, +4 );
-}
+  }
 
-void keyPressed() {
+  void keyPressed() {
   if (key == 'q') {
     exit();                           // press 'q' key to QUIT.
   }
   /* INSERT YOUR CODE HERE! */
-}
+  }
